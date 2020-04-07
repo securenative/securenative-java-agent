4a794eb8403dbb120becc5d90080a6fef32bbd78
Inbal Tako <inbal@securenative.com>
2020-04-07 17:37:13 +0300

    Add unit tests

---
+++ src/test/java/com/securenative/SecureNativeTest.java
@@ -0,0 +1,47 @@
+package com.securenative;
+
+import com.securenative.configurations.ConfigurationManager;
+import com.securenative.configurations.SecureNativeOptions;
+import com.securenative.exceptions.SecureNativeSDKException;
+import com.securenative.module.ModuleManager;
+import com.securenative.packagemanager.PackageManager;
+import com.securenative.packagemanager.SnPackage;
+import com.securenative.utils.Utils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SecureNativeTest {
+    @BeforeClass
+    public static void setup() {
+    }
+
+    @Test(expected = SecureNativeSDKException.class)
+    public void invalidInitialization() throws SecureNativeSDKException {
+        SecureNativeOptions config = ConfigurationManager.getConfig();
+        String PACKAGE_FILE_NAME = "/pom.xml";
+        SnPackage appPkg = PackageManager.getPackage(System.getProperty("user.dir").concat(PACKAGE_FILE_NAME));
+        ModuleManager moduleManager = new ModuleManager(appPkg);
+
+        new SecureNative(moduleManager, config);
+    }
+
+    @Test
+    public void validateMinSupportedJavaVersion() {
+        SecureNativeOptions config = ConfigurationManager.getConfig();
+        Assert.assertTrue(Utils.versionCompare(System.getProperty("java.version"), config.getMinSupportedVersion()) > 0);
+    }
+
+    @Test
+    public void readConfigurationFile() {
+        String PACKAGE_FILE_NAME = "/pom.xml";
+        SnPackage appPkg = PackageManager.getPackage(System.getProperty("user.dir").concat(PACKAGE_FILE_NAME));
+
+        String packageName = "com.securenative.java:securenative-java-agent";
+
+        Assert.assertNotNull(appPkg);
+        Assert.assertNotNull(appPkg.getDependenciesHash());
+        Assert.assertEquals(appPkg.getName(),packageName);
+        Assert.assertTrue(appPkg.getDependencies().length > 0);
+    }
+}
